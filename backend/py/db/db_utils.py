d9f888c2373166416627af6587cb91c67bb16997
Saint <max161125@gmail.com>
2025-05-16 12:56:11 +0300

    db add protection field types, fix db init

--- backend/py/db/db_utils.py
+++ backend/py/db/db_utils.py
@@ -3,14 +3,16 @@ from psycopg.rows import dict_row
 from db_config import DB_CONFIG
 
 def db_ensure_table(conn):
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS todos (
-            id SERIAL PRIMARY KEY,
-            text TEXT,
-            "dueDate" TEXT,
-            completed BOOLEAN DEFAULT FALSE
-        )
-    """)
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS todos (
+                id SERIAL PRIMARY KEY,
+                text VARCHAR(30) NOT NULL,
+                "dueDate" DATE NOT NULL,
+                completed BOOLEAN DEFAULT FALSE
+            )
+        """)
+    conn.commit()
 
 def db_get_connection():
     return psycopg.connect(**DB_CONFIG, row_factory=dict_row)
